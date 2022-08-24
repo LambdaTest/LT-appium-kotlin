0d008df522c6632e083cdd45705057e709040531
GarvitLT <garvits@GarvitShukija-MacBook-Pro.local>
2022-08-24 17:42:34 +0530

    Appium Kotlin

---
+++ src/test/kotlin/android.kt
@@ -0,0 +1,81 @@
+import io.appium.java_client.MobileBy
+import io.appium.java_client.MobileElement
+import io.appium.java_client.android.AndroidDriver
+import io.appium.java_client.android.AndroidElement
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.remote.DesiredCapabilities
+import java.net.URL
+
+class android {
+
+    @Test
+    fun `sample test`() {
+        val username = "LT_USERNAME"   //Add your LambdaTest username here
+        val accessKey = "LT_ACCESS_KEY"    //Add your LambdaTest accessKey here
+
+        var driver: AndroidDriver<MobileElement>? = null
+
+        val hub = URL("https://${username}:${accessKey}@mobile-hub.lambdatest.com/wd/hub")
+
+        val caps = DesiredCapabilities()
+        caps.setCapability("autoAcceptAlerts", true)
+        caps.setCapability("platformName", "Android")
+        caps.setCapability("deviceName", "Galaxy S20")
+        caps.setCapability("platformVersion", "11")
+        caps.setCapability("platformName", "Android")
+        caps.setCapability("isRealMobile", true)
+        caps.setCapability("app", "APP_URL") //Add the app (.apk) url here
+        caps.setCapability("deviceOrientation", "PORTRAIT")
+        caps.setCapability("build", "Kotlin Vanilla - Android")
+        caps.setCapability("name", "Sample Test Kotlin")
+        caps.setCapability("console", true)
+        caps.setCapability("network", false)
+        caps.setCapability("visual", true)
+        caps.setCapability("device log", true)
+
+
+        val app = AndroidDriver<AndroidElement>(hub, caps)
+
+        Thread.sleep(4000)
+
+        val color: MobileElement =
+            app.findElement(MobileBy.id("com.lambdatest.proverbial:id/color")) as MobileElement
+            color.click()
+
+        val text: MobileElement =
+            app.findElement(MobileBy.id("com.lambdatest.proverbial:id/Text")) as MobileElement
+            text.click()
+
+        val toast: MobileElement =
+            app.findElement(MobileBy.id("com.lambdatest.proverbial:id/toast")) as MobileElement
+            toast.click()
+
+        val notification: MobileElement =
+            app.findElement(MobileBy.id("com.lambdatest.proverbial:id/notification")) as MobileElement
+            notification.click()
+
+        val geo: MobileElement =
+            app.findElement(MobileBy.id("com.lambdatest.proverbial:id/geoLocation")) as MobileElement
+            geo.click()
+        Thread.sleep(5000)
+
+        val el3: MobileElement =
+            app.findElementByAccessibilityId("Home") as MobileElement
+
+        app.navigate().back()
+        Thread.sleep(2000)
+
+        val speedtest: MobileElement =
+            app.findElement(MobileBy.id("com.lambdatest.proverbial:id/speedTest")) as MobileElement
+            speedtest.click()
+        Thread.sleep(5000)
+
+        app.navigate().back()
+
+
+        if (app != null) {
+            app.quit()
+        };
+
+    }
+}
